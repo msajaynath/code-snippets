5393ddf0fb343b15e112a0730a1c63704375b54c
Ajay Nath MS <msajaynath@gmail.com>
2023-07-20 20:05:47 -0400

    Update test.ts

--- test.ts
+++ test.ts
@@ -71,3 +71,68 @@ export class CustomDropdownComponent {
     }
   }
 }
+
+
+export class CustomDropdownComponent {
+  @Input() options: string[] = [];
+  isOpen: boolean = false;
+  parentScrollContainer: HTMLElement;
+
+  constructor(private elRef: ElementRef) {}
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    if (!this.elRef.nativeElement.contains(event.target)) {
+      this.isOpen = false;
+    }
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: Event) {
+    this.adjustDropdownPosition();
+  }
+
+  toggleDropdown() {
+    this.isOpen = !this.isOpen;
+    this.adjustDropdownPosition();
+  }
+
+  selectOption(option: string) {
+    console.log(`Selected option: ${option}`);
+    this.isOpen = false;
+  }
+
+  setParentScrollContainer() {
+    // Find the parent scroll container of the dropdown
+    let parentElement = this.elRef.nativeElement.parentElement;
+    while (parentElement && parentElement.tagName !== 'BODY') {
+      const overflowY = window.getComputedStyle(parentElement).overflowY;
+      if (overflowY === 'auto' || overflowY === 'scroll') {
+        this.parentScrollContainer = parentElement;
+        return;
+      }
+      parentElement = parentElement.parentElement;
+    }
+  }
+
+  adjustDropdownPosition() {
+    if (this.isOpen) {
+      if (!this.parentScrollContainer) {
+        this.setParentScrollContainer();
+      }
+
+      if (this.parentScrollContainer) {
+        const dropdownButton = this.elRef.nativeElement.querySelector('.dropdown-toggle');
+        const dropdownMenu = this.elRef.nativeElement.querySelector('.dropdown-menu');
+        const buttonRect = dropdownButton.getBoundingClientRect();
+
+        const modalRect = this.parentScrollContainer.getBoundingClientRect();
+        const scrollTop = this.parentScrollContainer.scrollTop;
+        const offsetTop = buttonRect.top - modalRect.top + scrollTop;
+
+        dropdownMenu.style.top = offsetTop + buttonRect.height + 'px';
+        dropdownMenu.style.left = buttonRect.left + 'px';
+      }
+    }
+  }
+}
